7976fea661f62adb0f2da30f318464175d676f98
Kote_Animen <i.filyak@kvantron.com>
2021-12-29 15:13:56 +0300

    Добавьте файлы проекта.

---
+++ x_pract_8/x_pract_8/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace x_pract_8
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void birthDate_DateSelected(object sender, DateChangedEventArgs e)
+        {
+            DateTime today = DateTime.Now;
+            age.Text = "Возраст - " + Convert.ToString(today.Year - birthDate.Date.Year);
+        }        
+
+        private void btn_SetImage_Clicked(object sender, EventArgs e)
+        {
+            Image img = new Image();
+            img.Source = "dog.jpg";
+            
+            Images.Children.Add(img);            
+        }
+    }
+}
